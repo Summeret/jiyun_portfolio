4f7a76d38a689f7d51caf293e9efd280bbce5161
jiyun <jyiun27@gmail.com>
2025-07-15 18:10:05 +0900

    프로필 완성

--- scripts/index.js
+++ scripts/index.js
@@ -3,4 +3,15 @@ const wrap = new Swiper('#wrap',{
     mousewheel:true,
 })
 
+const nav = document.querySelectorAll('nav a');
+const header = document.querySelector('header')
+console.log(nav,header)
+
+nav.forEach((obj, idx)=>{
+    console.log(obj, idx)
+    obj.addEventListener('click',(e)=>{
+        e.preventDefault();
+        wrap.slideTo(idx, 1000);
+    })
+})
 
